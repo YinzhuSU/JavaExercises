6e9c08840eb18d376913329c9ad4bdd26c2cc950
YinzhuSU <31676438+YinzhuSU@users.noreply.github.com>
2018-02-09 00:03:18 -0500

    Update Two Sum again

---
+++ LeetCode/Two Sum/20180208TwoSum(return directly).java
@@ -0,0 +1,26 @@
+package PracticeLC;
+
+public class TwoSum {
+	public static void main(String[] args){
+		int[] num = {2, 2, 7, 11, 15, 2};
+		int target = 18;
+		System.out.println(twosum(num, target));
+	}
+	
+	public static String twosum(int[] num, int target){
+
+		int i;
+		int j;
+		String m;
+		for(i = 0; i < num.length-1; i++){		//why there's a warning?
+			for(j = i + 1; j < num.length; j++){
+				if(target - num[i] == num[j]){
+					return m = "The first indice is "+i+", the second indice is "+j+".";
+				}
+				continue;
+			}
+		}
+		return m = "There are no suitable two-numbers in the array.";
+	}
+}
+
